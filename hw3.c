96ae4b2cdad9f7274f31ceb7d2a9e1089844ffca
Amir Mairaj <amairaj@uci.edu>
2023-11-02 12:45:26 -0700

    stopping point

--- hw3.c
+++ hw3.c
@@ -1,32 +1,128 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct VirtualPage {
+    int valid;
+    int dirty;
+    int physical_page_number;
+    int content[8]; // 8 integers per page
+};
+
+struct PhysicalPage {
+   int content[8];
+   int page_number;
+};
+
+struct VirtualPage virt_mem[16];
+struct PhysicalPage physical_mem[4];
 
 
-
-int main(){
-    char input[80];
-    while(1){
-        printf(">");
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            perror("Error reading input");
-            exit(1);
+void initializeMemory() {
+    for (int i = 0; i < 16; i++) {
+        virt_mem[i].valid = 0;
+        virt_mem[i].dirty = 0;
+        virt_mem[i].physical_page_number = i; //physical page number
+        // initialize content (all 8 spots) to -1
+        for (int j = 0; j < 8; j++) {
+            virt_mem[i].content[j] = -1;
         }
-        char input[100];
-        scanf("%s", input)
-        char * tokens = strtok(input, " ");
-  
-        if(strcmp(input, "read") == 0){
-
-  
+    }
+    for (int i = 0; i < 4; i++) {
+        physical_mem[i].page_number = i;
+        // initialize content (all 8 spots) to -1
+        for (int j = 0; j < 8; j++) {
+            physical_mem[i].content[j] = -1;
         }
-        else if(strcmp(input, "write") == 0){
-
-        }
-        else if(strcmp(input, "showmain") == 0){
-
-        }
-        else{
-            printf("Invalid command\n");
-        }
-
     }
 }
-  
+void readMemory(int virtual_addy){
+    if (virtual_addy < 0 || virtual_addy > 127) {
+        printf("Invalid virtual address\n");
+        return;
+    }
+    else{
+        int page_number = virtual_addy / 8; // this is divided by 8 because there are 8 integers per page 
+        int offset = virtual_addy % 8; // offset tells us which integer in the page we want (0-7)
+        if (virt_mem[page_number].valid == 0) { // check if valid, valid meaning in physical memory
+            printf("A Page Fault Has Occurred\n");
+            printf("%d\n",virt_mem[page_number].content[offset]);
+            return;
+        }
+        else{
+            int content = virt_mem[page_number].content[offset];
+            printf("%d\n",content);
+            return;
+        }
+    }
+}
+
+
+void writeMemory(int virtual_addy, int data){
+    int page_number = virtual_addy / 8; // this is divided by 8 because there are 8 integers per page 
+
+
+    if (virtual_addy < 0 || virtual_addy > 127) {
+        printf("A Page Fault Has Occurred\n");
+        return;
+    }
+    else{
+        int desired_spot = virtual_addy % 8; // offset tells us which integer in the page we want (0-7)
+        int actual_page_in_mem = page_number / 4;
+        int actual_place_in_mem = page_number % 4;
+        virt_mem[page_number].content[desired_spot] = data;
+        virt_mem[page_number].dirty = 1;
+        virt_mem[page_number].valid = 1;
+        virt_mem[page_number].physical_page_number = actual_page_in_mem;
+        physical_mem[actual_page_in_mem].content[actual_place_in_mem] = data;
+        return;
+        }
+    }
+
+
+void showMainMemory(int physical_page_addy){
+    if (physical_page_addy < 0 || physical_page_addy > 3) {
+        printf("Invalid physical page number\n");
+        return;
+    }
+    int numbering = physical_page_addy * 8;
+    for (int j = 0; j < 8; j++) {
+        printf("%d: %d\n",numbering + j ,physical_mem[physical_page_addy].content[j]);
+    }
+}
+
+void showPageTable(){
+    //virtual page number,Valid bit, Dirty bit, and Page Frame Number
+    for (int i = 0; i < 16; i++) {
+        printf("%d:%d:%d:%d\n",i,virt_mem[i].valid,virt_mem[i].dirty,virt_mem[i].physical_page_number);
+    }
+}
+
+int main() {
+    initializeMemory();
+    while (1) {
+        printf("> ");
+        char command[50];
+        int virtual_address, data, ppn;
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            printf("Error reading command\n");
+        } else if  (sscanf(command, "read %d", &virtual_address) == 1) {
+            readMemory(virtual_address);
+        } else if (sscanf(command, "write %d %d", &virtual_address, &data) == 2) {
+            writeMemory(virtual_address, data);
+        } else if (sscanf(command, "showmain %d", &ppn) == 1) {
+            showMainMemory(ppn);
+        } else if (strcmp(command, "showptable\n") == 0) {
+            showPageTable();
+        } else if (strcmp(command, "quit\n") == 0) {
+            exit(0);
+            break;
+        } 
+        else {
+            printf("Invalid command\n");
+        }
+    }
+    return 0;
+}
